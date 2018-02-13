a919770ee544b09b3315a27f274593109737c5ba
convoluted <duminydaniel@gmail.com>
2018-02-14 03:20:43 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+<meta http-equiv="Refresh" content="300,  URL=https://google.com">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<Title> ...  Upgrade Website in Progress ... </title>
+</head>
+
+<body>
+<font color=red>
+<div class= image><img src="https://cdn.geckoandfly.com/wp-content/uploads/2016/05/uplifting-inspirational-quotes-25.jpg"> </div>
+<h1 align=center></h1>
+
+
+
+
+
+
+</body>
+<style>
+   .image{
+   height: 300px;
+   width: 400;
+}
+
+</html>
+
+
